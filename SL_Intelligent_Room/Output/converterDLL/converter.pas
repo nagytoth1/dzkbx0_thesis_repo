06b0b0b4fe2918b0b8700549185504ce3205b314
Nagy-Tóth Bence <dzkbx0@std.uni-eszterhazy.hu>
2022-11-11 08:58:29 +0100

    setTurnForEachDevice written with json_source
    Integrated JSONParsing into method setTurnForEachDevice, according to my theory from now on what setRound was
    is callable and working with JSON, meaning it could be used to grant parameters to devices in a standardized way.
    Added constants for new error codes. Via error codes we can throw typized (custom) exceptions in C# therefore it should be easier to debug the DLL in the future.

--- SL_Intelligent_Room/Output/converterDLL/converter.pas
+++ SL_Intelligent_Room/Output/converterDLL/converter.pas
@@ -12,11 +12,14 @@ const
   MAX_DEVICECOUNT = 100;
   UZESAJ = WM_USER + 0;
   CONVERTERDLL_PATH = 'relay.dll';
-  DEV485_EMPTY = 255;
-  DEV485_ALREADY_FILLED = 254;
-  EXIT_SUCCESS = 0;
   PRODUCER = 'Somodi László';
   MANUFACTURER = 'Pluszs Kft.';
+  //error codes instead of exceptions - the cause thrown exceptions cannot be detected by C# PInvoke
+  DEV485_EMPTY = 255;
+  DEV485_ALREADY_FILLED = 254;
+  TURNNUM_OUTOFBOUNDS = 253;
+  DEVCOUNT_IDENTITY_ERROR = 252;
+  EXIT_SUCCESS = 0;
 var
   drb485: integer;
   dev485:DEVLIS; //where is it set?
@@ -34,6 +37,8 @@ function setTurnForEachDevice(turn : byte):integer; stdcall; external CONVERTERD
 procedure Split (const Delimiter: Char; Input: string; const Strings: TStrings);
 procedure RemoveSpecialChars(var str : string); external CONVERTERDLL_PATH;
 function convertJSONToDeviceList(json_source: string):DEVLIS; external CONVERTERDLL_PATH; //param: PDEVLIS?
+function extractValueFromJSONField(const json_element: string, const key: string):string;
+function reduceJSONSourceToElements(const json_source: string):TStringList;
  
 implementation
 //Használata: Split('^', Forrás, Eredmény);
--- SL_Intelligent_Room/Output/converterDLL/relay/relay.dpr
+++ SL_Intelligent_Room/Output/converterDLL/relay/relay.dpr
@@ -30,7 +30,7 @@ begin
   dev485[2].azonos := $4004;
   dev485[2].produc := 'Teszt Elek';
   dev485[2].manufa := 'Valaki Zrt.';
-  writeln('Dev485 filled  with static elements');
+  writeln('Dev485 filled  with static actDeviceSettings');
   result := EXIT_SUCCESS;
 end;
 
@@ -77,32 +77,6 @@ procedure RemoveSpecialChars(var str : string); //string is given by as referenc
 end;
 //question: should dev485 be rather given in as parameter?
 //in C# we don't need this, we will implement it for C#-environment
-function extractValueFromJSONField(const json_element: string, const key: string):string;
-var 
-	value:string;
-	jsonField: TStringList;
-begin
-	value := '';
-	jsonField := TStringList.Create();
-	//jsonElement looks like this: azonos:16388
-	Split(':', json_element, jsonField);
-	if jsonField[0] = fieldName then 
-	 	value := jsonField[1]; //'16388'
-	result := value;
-end;
-
-function reduceJSONSourceToElements(const json_source: string):TStringList;
-var 
-	jsonArrayElements: TStringList;
-begin
-	jsonArrayElements := TStringList.Create();
-	
-	RemoveSpecialChars(json_source);
-  	Split(',', json_source, jsonArrayElements);
-	
-	result := jsonArrayElements; //'azonos:16388, '
-end;
-
 function convertJSONToDeviceList(json_source: string):DEVLIS; //returns array dev485
 var
   //input looks like this: [{"azonos" : 16388, "tipus" : "L"},{"azonos": 120, "tipus" : "0"}, ... ]
@@ -111,21 +85,25 @@ var
   i : integer;
   k : integer;
   dev485 : DEVLIS;
+  dev_azonos: word;
 begin
   SetLength(dev485, MAX_DEVICECOUNT);
-  jsonArrayElements := ReduceJSONToElements(json_source);
+  jsonArrayElements := reduceJSONSourceToElements(json_source);
   k := 0; //the index of dev485 gets incremented only if field 'azonos' is found with its value
   for i := 0 to jsonArrayElements.Count - 1 do
   begin
     json_element := jsonArrayElements[i];
     writeln('json_element ' + json_element); 
-	json_value := extractValueFromJSONField(json_element, 'azonos');
-	
-	if json_value = '' then
+    json_value := extractValueFromJSONField(json_element, 'azonos');
+    if json_value = '' then
 		continue;
-
-	//TODO: what if json_value can not be parsed to int?
-    dev485[k].azonos := StrToInt(json_value); //dev485[k].azonos gets the value 16388 as an integer
+	
+	// if the given string does not represent a valid integer (invalid), result is -1
+    dev_azonos := strToIntDef(json_value, -1);
+    if dev_azonos = -1 then 
+        showmessage(Format('Invalid deviceID %s', [json_value]));
+        continue;
+    dev485[k].azonos := dev_azonos; //dev485[k].azonos gets the value '16388' as an integer
     //these 2 fields are constants
     dev485[k].produc := PRODUCER; 
     dev485[k].manufa := MANUFACTURER;
@@ -135,8 +113,32 @@ begin
   writeln('Array dev485 has been created from JSON-string successfully!');
   result := dev485;
 end;
+function extractValueFromJSONField(const json_element: string, const key: string):string;
+var 
+	value:string;
+	jsonField: TStringList;
+begin
+	value := '';
+	jsonField := TStringList.Create();
+	//jsonElement looks like this: azonos:16388
+	Split(':', json_element, jsonField);
+	//if jsonField fails to be split, result value is going to be ''
+	if Length(jsonField) <> 0 and jsonField[0] = fieldName then 
+	 	value := jsonField[1]; //'16388'
+	result := value;
+end;
+function reduceJSONSourceToElements(const json_source: string):TStringList;
+var 
+	jsonArrayElements: TStringList;
+begin
+	jsonArrayElements := TStringList.Create();
+	
+	RemoveSpecialChars(json_source);
+  	Split(',', json_source, jsonArrayElements);
+	result := jsonArrayElements; //'azonos:16388, tipus:"L" ...'
+end;
 
-function convertDeviceListToXML(const outPath:string): Byte;
+function convertDeviceListToXML(const outPath:string): byte;
 var
   XML : IXMLDOCUMENT;
   RootNode : IXMLNODE;
@@ -175,7 +177,7 @@ var
 begin
   res := SLDLL_Felmeres();
   showmessage(Format('Felmeres eredmenye %d', [res]));
-  res := SLDLL_Listelem(@dev485);
+  res := SLDLL_Listelem(@dev485); //sets dev485 and drb485 as well
   showmessage(Format('ListElem eredmenye %d', [res]));
   
   if(not Assigned(dev485)) then //if dev485 == null
@@ -200,14 +202,16 @@ begin
   result := res;
 end;
 
-function setTurnForEachDevice(turn : byte, json_data: string):integer;
+function setTurnForEachDevice(turn : byte, json_source: string):integer;
 begin
 var
-  RES, i: integer;
-  EDB: integer;
-  prgString, prgLine: string;
-  actDeviceType: string;
-  actDeviceSettings: TStringList;
+	validationResult := byte;
+	i: integer;
+	devCount: integer;
+	jsonArrayElements: TStringList;
+	json_element: string;
+	actDeviceType: string;
+	actDeviceSettings: TStringList;
 begin
 	//this method will be used in a loop, therefore "out of bounds" values can not get assigned
 	//only used for checking for errors
@@ -219,87 +223,114 @@ begin
   	end;
 
 	//decode the JSON of the current turn (type + settings fields)
-	actDeviceType := extractValueFromJSONField(jsonField)
-	prgLine := SLProgram[turn];
-	programElements := TStringList.Create();
-	Split('/', prgLine, programElements);
-
-	EDB := programElements.Count; //count of array - XMLNodes or JSON-array length
-	if EDB <> drb485: //if the array is not the same size as length of devicelist, we can't run the method properly
+	jsonArrayElements := reduceJSONSourceToElements(json_source);
+	devCount := jsonArrayElements.Count; //count of array - XMLNodes or JSON-array length
+	
+	if devCount <> drb485: //if the array is not the same size as length of devicelist, we can't run the method properly
 	begin
 		Result := DEVCOUNT_IDENTITY_ERROR;
 		exit;
 	end;
-		
-	for i := 0 to EDB - 1 do
+
+	for i := 0 to devCount - 1 do
 	begin
-		try
-		elements := TStringList.Create();
-		prgString := programElements[i];
-		Split('|', prgString, elements); //splits the settings for different devices
-
-		devList[i].azonos := dev485[i].azonos;
-		case(dev485[i].azonos AND $c000) of
-		SLLELO: //LEDLight
-		begin
-			setLEDDevice(
-				strToInt(elements[0]), 
-				strToInt(elements[1]), 
-				strToInt(elements[2]), 
-				strToInt(elements[3]));
-		end;
-		SLNELO: //LEDArrow
-		begin
-			setLEDDevice(
-				strToInt(elements[0]), 
-				strToInt(elements[1]), 
-				strToInt(elements[2]), 
-				strToInt(elements[3]));
-		end;
-		SLHELO: //Speaker
+		json_element := jsonArrayElements[i]; //loads the actual device
+    	writeln('json_element ' + json_element); 
+		
+		actDeviceType := extractValueFromJSONField(jsonField, 'type'); //gets the type of the device
+		Split('|', extractValueFromJSONField(jsonField, 'settings'), actDeviceSettings);
+    	
+		if(validateExtractedDeviceValues(actDeviceType, actDeviceSettings) <> 0) then
 		begin 
-			setSpeaker(
-				strToInt(elements[0]), 	//volume, e.g: 10
-				strToInt(elements[1]), 	//index from table e.g: 1
-				strToInt(elements[2])); //length
+			continue;
 		end;
-
+		
+		devList[i].azonos := dev485[i].azonos;
+		setDeviceByType(actDeviceType);
 		end; //case
-		except
-		showmessage('Elem beállítási hiba');
-		end;
-
 	end; //for
 	try
-		RES := SLDLL_SetLista(EDB, devList);
+		result := SLDLL_SetLista(devCount, devList);
 	except
-		showmessage('Eszközbeállítási hiba: ' + inttostr(RES));
+		showmessage('SLDLL_SetLista resulted in error: ' + inttostr(RES));
 	end; //try
 end;
-function setLEDDevice(i, red, green, blue, direction:byte):integer;
+
+function validateExtractedDeviceValues(const actDeviceType, actDeviceSettings: string):byte;
 begin
-	devList[i].vilrgb.rossze := red;
-	devList[i].vilrgb.gossze := green;
-	devList[i].vilrgb.bossze := blue;
-	devList[i].nilmeg := direction;
+	if actDeviceType = '' then
+	begin
+		//result := DEVTYPE_UNDEFINED;
+		writeln(Format('%d DEVTYPE_UNDEFINED', [i]));
+		result := DEVTYPE_UNDEFINED;
+		exit;
+	end;
+	if length(actDeviceSettings) < 3 then
+	begin
+		writeln(Format('%d DEVSETTINGS_INVALID_FORMAT', [i]));
+		result := DEVSETTINGS_INVALID_FORMAT;
+		exit;
+	end;
+	result := 0;
+end;
+procedure setDeviceByType(const actDeviceType: string);
+begin
+	case(actDeviceType) of
+		'L': setLEDDevice( 	//LEDLight
+				strToIntDef(actDeviceSettings[0], 0), 	//red
+				strToIntDef(actDeviceSettings[1], 0), 	//green
+				strToIntDef(actDeviceSettings[2], 0), 	//blue
+				strToIntDef(actDeviceSettings[3], 0)); 	//direction
+		'N': setLEDDevice( 	//LEDArrow
+				strToIntDef(actDeviceSettings[0], 0), 	//red
+				strToIntDef(actDeviceSettings[1], 0), 	//green
+				strToIntDef(actDeviceSettings[2], 0), 	//blue
+				strToIntDef(actDeviceSettings[3], 0));	//direction
+		'H': setSpeaker( //Speaker
+				strToIntDef(actDeviceSettings[0], 0), 	//volume, e.g: 10
+				strToIntDef(actDeviceSettings[1], 0), 	//index from table e.g: 1
+				strToIntDef(actDeviceSettings[2], 0)); 	//length
+end;
+function setLEDDevice(i, red, green, blue, direction:byte):byte;
+var 
+	code : byte;
+begin
+	code := 0;
+	try
+		devList[i].vilrgb.rossze := red;
+		devList[i].vilrgb.gossze := green;
+		devList[i].vilrgb.bossze := blue;
+		devList[i].nilmeg := direction;	
+	except
+		showmessage('LEDDevice setting failed.');
+		code := DEVSETTING_FAILED;
+	finally
+		result := code;
 end;
 function setSpeaker(i, volume, id, length:byte):integer;
+var 
+	code : byte;
 begin
-	devList[i].handrb := 1;
-	devList[i].hantbp := @H; //array for sound settings - 
-	//TODO: do we need array?
-	H[0].hangho := length; //100;
-	H[0].hangso := id; //1;
-	H[0].hanger := volume; //10;
+	code := 0;
+	try
+		devList[i].handrb := 1;
+		devList[i].hantbp := @H; //array for sound settings - 
+		//TODO: do we need array?
+		H[0].hangho := length; //100;
+		H[0].hangso := id; //1;
+		H[0].hanger := volume; //10;	
+	except
+		showmessage('LEDDevice setting failed.');
+		code := DEVSETTING_FAILED;
+	finally
+		result := code;
+	end;
 end;
-
 {$R *.res}
-
 exports openDLL, 
-detectDevices, 
-convertDeviceListToJSON, 
-fillDeviceListWithDevices,
-convertDeviceListToXML;
-
+		detectDevices, 
+		convertDeviceListToJSON, 
+		fillDeviceListWithDevices,
+		convertDeviceListToXML;
 begin
 end.
